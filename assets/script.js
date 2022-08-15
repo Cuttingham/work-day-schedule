acc7a6e5dcc6668fc370ad59fd9d0df46d5766d2
Jacob Karla <jacob.l.karla@gmail.com>
2022-08-14 23:12:52 -0400

    Now properly colors background based on time of day, added button functionality, does not presently save data

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,29 @@
+var saveBtn =$('.saveBtn')
 var today=moment();
 $("#currentDay").text(today.format("MMM Do, YYYY")); //shows current date at top of screen, works good
 
+
+const d =new Date();
+var currentTime =d.getHours();
+
+console.log(currentTime);
+
+$(".time-block").each(function (){
+var comparisonHour = parseInt($(this).attr("id"));   //grabs id element, turns into integer, compares it to current hour
+
+if (comparisonHour<currentTime){
+    $(this).addClass("past");
+}
+else if(comparisonHour===currentTime){
+    $(this).addClass("present");
+}
+else{
+    $(this).addClass("future");
+}
+}
+);
+
+saveBtn.click(function(){
+    console.log("Clicked:"); //this button works
+});
 
